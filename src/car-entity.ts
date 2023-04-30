0a9e4844fbfccd4ecf882434a471a4db304fabe7
Jorginho <jorgejunior.jpj2016@gmail.com>
2023-04-29 23:51:01 -0300

    ✨ buil: Create car domain class

---
+++ src/car-entity.ts
@@ -0,0 +1,11 @@
+export class Car {
+  public brand: string
+  public model: string
+  public year: string
+
+  private constructor(brand: string, model: string, year: string) {
+    this.brand = brand
+    this.model = model
+    this.year = year
+  }
+}
