6ba69ffbcb1470b463f8e4ffc3859c986582e2d1
Jorginho <jorgejunior.jpj2016@gmail.com>
2023-04-30 02:07:52 -0300

    ⚰️ chore: Removes dead code

--- src/car-entity.spec.ts
+++ src/car-entity.spec.ts
@@ -2,12 +2,6 @@ import { CarData } from "./car-data"
 import { Car } from "./car-entity"
 
 describe('CAR ENTITY TESTS', () => {
-  // test.skip('Should create a car with contructor', () => {
-  //   const car = new Car('CarBrand', 'CarModel', '2015')
-
-  //   expect(car).toBeInstanceOf(Car)
-  // })
-
   test('Should create a car with factory method', () => {
     const carData: CarData = {
       brand: 'Fiat',
