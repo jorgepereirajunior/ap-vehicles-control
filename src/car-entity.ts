967639cb9db3a2bbae7bc51aa5a9238559f197ae
Jorginho <jorgejunior.jpj2016@gmail.com>
2023-05-02 06:31:01 -0300

    🏷️ chore: Change some types

--- src/car-entity.ts
+++ src/car-entity.ts
@@ -1,25 +1,43 @@
+import { Brand } from "./brand"
+import { Model } from "./model"
+import { Bodywork } from "./bodywork"
+import { GearType } from "./geartype"
+
 import { CarData } from "./car-data"
 
 export class Car {
-  public brand: string
-  public model: string
-  public bodywork: string
+  public brand: Brand
+  public model: Model
+  public bodywork: Bodywork
   public engine: string
-  public gearType: string
+  public gearType: GearType
   public year: string
   public color: string
 
-  constructor(carData: CarData) {
-    this.brand = carData.brand
-    this.model = carData.model
-    this.bodywork = carData.bodywork
-    this.engine = carData.engine
-    this.gearType = carData.engine
-    this.year = carData.year
-    this.color = carData.color
+  constructor(
+    brand: Brand,
+    model: Model,
+    bodywork: Bodywork,
+    engine: string,
+    gearType: GearType,
+    year: string,
+    color: string
+  ) {
+    this.brand = brand
+    this.model = model
+    this.bodywork = bodywork
+    this.engine = engine
+    this.gearType = gearType
+    this.year = year
+    this.color = color
   }
 
   public static create(carData: CarData) {
-    return new Car(carData)
+    const brand = Brand.create(carData.brand)
+    const model = Model.create(carData.model)
+    const bodywork = Bodywork.create(carData.bodywork)
+    const gearType = GearType.create(carData.gearType)
+
+    return new Car(brand, model, bodywork, carData.engine, gearType, carData.year, carData.color)
   }
 }
